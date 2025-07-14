d3167e0f7439335b16054c8e3f7567d5a119850a
Jan-Lukas Schröder <st177404@stud.uni-stuttgart.de>
2025-07-14 11:15:22 +0200

    Update calculate_aerodynamic_stiffness to include alpha_zero parameter for torque calculations

--- analysis/functions/calculate_aerodynamic_stiffness.m
+++ analysis/functions/calculate_aerodynamic_stiffness.m
@@ -1,4 +1,4 @@
-function stiffness = calculate_aerodynamic_stiffness(method, delta__rad, axis_dir_B, component, bodies, bodies_rotation_angles__rad, model, lut_data)
+function stiffness = calculate_aerodynamic_stiffness(method, alpha_zero, delta__rad, axis_dir_B, component, bodies, bodies_rotation_angles__rad, model, lut_data)
 %% calculate_aerodynamic_stiffness - calculate the aerodynamic stiffness using numerical differentiation
 %
 % stiffness = calculate_aerodynamic_stiffness(method,...
@@ -14,6 +14,7 @@ function stiffness = calculate_aerodynamic_stiffness(method, delta__rad, axis_di
 % Inputs:
 %   method: string specifying the numerical differentiation method to use
 %           Options: 'central', 'five_point_stencil', 'seven_point_stencil', 'forward', 'backward' 
+%   alpha_zero: angle the stiffness is calculated at, in radians
 %   delta: step size for numerical differentiation
 %   axis_dir_B: 3x1 vector specifying the rotation axis in the body frame
 %   component: index of the torque component to differentiate (1 = x, 2 = y, 3 = z)
@@ -27,6 +28,7 @@ function stiffness = calculate_aerodynamic_stiffness(method, delta__rad, axis_di
 %   calculated at the specified rotation axis and component
     arguments
         method (1,1) string {mustBeMember(method, {'central', 'five_point_stencil', 'seven_point_stencil', 'forward', 'backward'})}
+        alpha_zero (1,1) {mustBeNumeric, mustBeReal};
         delta__rad (1,1) {mustBeNumeric, mustBeReal, mustBePositive};
         axis_dir_B (3,1) {mustBeNumeric, mustBeReal};
         component (1,1) {mustBeNumeric, mustBeReal, mustBeInteger, mustBeGreaterThanOrEqual(component, 1), mustBeLessThanOrEqual(component, 3)};
@@ -39,38 +41,38 @@ function stiffness = calculate_aerodynamic_stiffness(method, delta__rad, axis_di
     switch method
         case 'central'
             % Central difference: f'(x) ≈ (f(x+h) - f(x-h)) / (2h)
-            torque_plus = compute_aerodynamics_at_angle(delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_minus = compute_aerodynamics_at_angle(-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_plus = compute_aerodynamics_at_angle(alpha_zero+delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_minus = compute_aerodynamics_at_angle(alpha_zero-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
             stiffness = (torque_plus(component) - torque_minus(component)) / (2 * delta__rad);
             
         case 'five_point_stencil'
             % Five-point stencil: f'(x) ≈ (-f(x+2h) + 8f(x+h) - 8f(x-h) + f(x-2h)) / (12h)
-            torque_1 = compute_aerodynamics_at_angle(-2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_2 = compute_aerodynamics_at_angle(-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_3 = compute_aerodynamics_at_angle(delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_4 = compute_aerodynamics_at_angle(2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_1 = compute_aerodynamics_at_angle(alpha_zero-2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_2 = compute_aerodynamics_at_angle(alpha_zero-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_3 = compute_aerodynamics_at_angle(alpha_zero+delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_4 = compute_aerodynamics_at_angle(alpha_zero+2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
             stiffness = (-torque_4(component) + 8*torque_3(component) - 8*torque_2(component) + torque_1(component)) / (12 * delta__rad);
             
         case 'seven_point_stencil'
-            % Seven-point stencil: f'(x) ≈ (-f(x-3h) + 9f(x-2h) - 45f(x-h) + 45f(x+h)  9f(x+2h) + f(x+3h)) / (60h)
-            torque_1 = compute_aerodynamics_at_angle(-3*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_2 = compute_aerodynamics_at_angle(-2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_3 = compute_aerodynamics_at_angle(-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_4 = compute_aerodynamics_at_angle(delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_5 = compute_aerodynamics_at_angle(2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_6 = compute_aerodynamics_at_angle(3*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            % Seven-point stencil: f'(x) ≈ (-f(x-3h) + 9f(x-2h) - 45f(x-h) + 45f(x+h) - 9f(x+2h) + f(x+3h)) / (60h)
+            torque_1 = compute_aerodynamics_at_angle(alpha_zero-3*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_2 = compute_aerodynamics_at_angle(alpha_zero-2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_3 = compute_aerodynamics_at_angle(alpha_zero-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_4 = compute_aerodynamics_at_angle(alpha_zero+delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_5 = compute_aerodynamics_at_angle(alpha_zero+2*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_6 = compute_aerodynamics_at_angle(alpha_zero+3*delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
             stiffness = (-torque_1(component) + 9*torque_2(component) - 45*torque_3(component) + 45*torque_4(component) - 9*torque_5(component) + torque_6(component)) / (60 * delta__rad);
             
         case 'forward'
             % Forward difference: f'(x) ≈ (f(x+h) - f(x)) / h
-            torque_0 = compute_aerodynamics_at_angle(0, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_plus = compute_aerodynamics_at_angle(delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_0 = compute_aerodynamics_at_angle(alpha_zero, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_plus = compute_aerodynamics_at_angle(alpha_zero+delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
             stiffness = (torque_plus(component) - torque_0(component)) / delta__rad;
             
         case 'backward'
             % Backward difference: f'(x) ≈ (f(x) - f(x-h)) / h
-            torque_0 = compute_aerodynamics_at_angle(0, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
-            torque_minus = compute_aerodynamics_at_angle(-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_0 = compute_aerodynamics_at_angle(alpha_zero, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
+            torque_minus = compute_aerodynamics_at_angle(alpha_zero-delta__rad, axis_dir_B, bodies, bodies_rotation_angles__rad, model, lut_data);
             stiffness = (torque_0(component) - torque_minus(component)) / delta__rad;
             
         otherwise
